a087865f32054b3663e5e864e8b9ed538a591201
PoorvikaN <paapurvikaa88888@gmail.com>
2025-06-30 14:11:01 +0530

    Add files via upload

---
+++ ReadKeyboard.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.util.*;
+
+
+public class ReadKeyboard
+{
+      public static void main(String arg[])
+      {
+           Scanner sc=new Scanner(System.in);
+           String x=sc.nextLine();
+           System.out.println(x);
+
+       }
+}
