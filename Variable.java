23405ee0368acf7239e28be817f202243f880e22
PoorvikaN <paapurvikaa88888@gmail.com>
2025-07-02 14:24:35 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+
+class Variable
+{
+      public static void main(String arg[])
+      {
+            byte b=127;
+            Short s=300;
+            int i=100;
+            float f=25.3f;
+            char c='A';
+            System.out.println(b);
+            System.out.println(s);
+            System.out.println(i);
+            System.out.println(f);
+      }
+}
---
+++ Variables.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+
+class Variables
+{
+      public static void main(String arg[])
+      {
+           int x;
+           int X;
+           int roll_Number$student;
+           int _x;
+           int String=10;
+           System.out.println(String);
+           int rollNumberOfStud;
+           float averageMarkOfClass;
+       }
+
+}
