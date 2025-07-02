2aa8cc22ec6400bacee36def1730ad63feebfdcb
PoorvikaN <paapurvikaa88888@gmail.com>
2025-07-02 14:23:22 +0530

    Add files via upload

---
+++ Literals.java
@@ -0,0 +1,23 @@
+import java.lang.*;
+
+
+class Literals
+{
+      public static void main(String arg[])
+      {
+           byte b1=10;
+           byte b2=0b1010;
+           byte b3=012;
+           byte b4=0XA;
+           long l=999_999_999;
+           float f=123_456.56f;
+           double d=12.56d;
+           System.out.println(b1); 
+           System.out.println(b2); 
+           System.out.println(b3); 
+           System.out.println(b4); 
+           System.out.println(l);
+           System.out.println(f);
+           System.out.println(d);
+       }
+}
