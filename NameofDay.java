3f6c20bbdd8b99e419312f4634bff205adeb7179
Poorvika N <paapurvikaa88888@gmail.com>
2025-07-06 15:53:47 +0530

    Add files via upload

---
+++ NameofDay.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class NameofDay {
+
+    public static void main(String[] args) {
+       Scanner scan=new Scanner(System.in);
+       int day;
+       System.out.println("Enter a number");
+     day=scan.nextInt();
+     
+     if(day==1)
+     { 
+         System.out.println("MONDAY");
+     }
+     else if(day==2)
+     {
+         System.out.println("TUESDAY");
+     }
+     else if(day==3)
+     {
+         System.out.println("WEDNESDAY");
+     }
+      else if(day==4)
+     {
+         System.out.println("THURSDAY");
+     }
+      else if(day==5)
+     {
+         System.out.println("FRIDAY");
+     }
+      else if(day==6)
+     {
+         System.out.println("SATURDAY");
+     }
+      else if(day==7)
+     {
+         System.out.println("SUNDAY");         
+     }
+     else
+      {
+          System.out.println("Invalid");
+      }
+    }
+}
+      
