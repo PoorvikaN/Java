f476d3ae4d0293afc56b26e21a74e6ac4af82475
Poorvika N <paapurvikaa88888@gmail.com>
2025-07-08 14:33:07 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Fibonacci
+{ 
+   public static void main(String[] args)
+   {
+        Scanner sc=new Scanner(System.in);
+      
+        System.out.println("Program to print Fibonacci Series");
+        System.out.println("Enter n value");
+        int n=sc.nextInt();
+ 
+        int a=0,b=1,c;
+        System.out.print(a+","+b+",");
+
+        for(int i=0; i<n-2; i++)
+         {
+            c=a+b;
+            System.out.print(c+",");       
+            a=b;
+            b=c;
+          }
+       } 
+    }
