1163bd54ab003933667ea91886337e1805de7e06
Poorvika N <paapurvikaa88888@gmail.com>
2025-07-14 13:24:05 +0530

    Add files via upload

---
+++ FindPrime.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class FindPrime {
+    static boolean isPrime(int n) 
+    {
+        for(int i=2;i<n/2;i++)
+        {
+            if(n%i==0)
+                return false;
+        }
+        return true;
+    } 
+    public static void main(String[] args) {
+       System.out.println(isPrime(19));
+    }
+    
+}
