e0c458c70f674dcd585d671711fe0879a81cca2b
Poorvika N <paapurvikaa88888@gmail.com>
2025-07-14 16:18:12 +0530

    Add files via upload

---
+++ MethodValidate.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class MethodValidate {
+    boolean validate(String name)
+    {
+        return name.matches("[a-zA-Z\\s]+");
+    }
+    boolean validate (int age)
+    {
+        return age>=3&& age<=15;
+    }
+    
+
+    public static void main(String[] args) {
+       
+    }
+    
+}
