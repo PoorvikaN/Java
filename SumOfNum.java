5844f4636c1063e13a981d3701e91cd95c0847cf
Poorvika N <paapurvikaa88888@gmail.com>
2025-07-15 12:08:03 +0530

    Add files via upload

---
+++ SumOfNum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class SumOfNum
+{
+   public static void main(String[] args)
+    {
+      double s=0;
+      for(String x:args)
+      {
+          //if(x.matches("[0-9\\.]+"))
+          s=s+Double.parseDouble(x);
+       }
+    System.out.println("Sum is "+s);
+  }
+}
