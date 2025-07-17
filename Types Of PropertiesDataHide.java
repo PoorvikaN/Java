55fae434d9d8cefcfdeb2060a67ffc35567efdff
Poorvika N <paapurvikaa88888@gmail.com>
2025-07-17 11:53:28 +0530

    Add files via upload

---
+++ Types Of PropertiesDataHide.java
@@ -0,0 +1,42 @@
+Types Of Properties
+
+READ AND WRITE METHOD
+
+class Rectangle
+{
+ private double length;
+
+ public getLength()
+ {
+       return length;
+  }
+ public void setLength(double 1)
+  {
+     length=1;
+  }
+}
+
+READ ONLY
+
+class Student
+{
+ private double roll;
+
+ public int getRollNumber()
+ {
+       return roll;
+  }
+}
+
+WRITE ONLY
+
+class Producer
+{
+ private double sharedData;
+
+ public setData(int d)
+ {
+       sharedData=d;
+  }
+}
+
